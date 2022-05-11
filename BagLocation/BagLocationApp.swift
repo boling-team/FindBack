8c667f9830d678e5a227f15ba31cc49073ac82f3
Agus Budianto <agusbudianto@Aguss-MacBook-Pro.local>
2022-05-11 20:34:48 +0700

    innit project

---
+++ BagLocation/BagLocationApp.swift
@@ -0,0 +1,20 @@
+//
+//  BagLocationApp.swift
+//  BagLocation
+//
+//  Created by Agus Budianto on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BagLocationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
